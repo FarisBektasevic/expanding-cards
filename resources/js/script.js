8b486c9dd4556642d96fe274cb6528df077859fb
FarisBektasevic <bektasevicfaris@gmail.com>
2021-08-21 18:01:38 +0200

    ...

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,3 @@
-// const panels = document.querySelectorAll('.panel')
-
-// panels.forEach( (panel) => {
-//     panel.addEventListener('click', () => {
-//         removeActiveClasses(panel)
-//         panel.classList.add('active')
-//     })
-    
-// })
-
-// function removeActiveClasses (panel) {
-//     panels.forEach( (panel) => {
-//         panel.classList.remove('active')
-//     })
-// }
 
 const panels = document.querySelectorAll('.panel')
 
